d25e4e6f0090a3306e3a0721c8e452de9fb257c4
Joe <xianqi@ucsb.edu>
2022-04-29 13:26:48 -0700

    entity

--- src/main/java/edu/ucsb/cs156/example/entities/Article.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Article.java
@@ -20,6 +20,7 @@ import lombok.Builder;
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String url;
     private String explanation;
