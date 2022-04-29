82751503727c4fa44d99579cdbfe9a740c0c5440
Joe <xianqi@ucsb.edu>
2022-04-29 13:07:50 -0700

    new reposity

--- src/main/java/edu/ucsb/cs156/example/entities/Article.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Article.java
@@ -3,7 +3,9 @@ package edu.ucsb.cs156.example.entities;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.Builder;
 @Entity(name = "Article")
 public class Article {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String title;
     private String url;
     private String explanation;
