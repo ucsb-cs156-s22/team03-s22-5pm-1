6b7be614a4d50642aaec5e333bc0d6d775cbde43
rishabhpoikayil <rishabh.poikayil@gmail.com>
2022-05-17 07:50:47 -0700

    rp - placholder page replaced with Recommendation Table

---
+++ frontend/src/main/components/Recommendations/RecommendationsTable.js
@@ -0,0 +1,72 @@
+import OurTable from "main/components/OurTable";
+// import { useBackendMutation } from "main/utils/useBackend";
+// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+// import { hasRole } from "main/utils/currentUser";
+
+export default function RecommendationsTable({ recommendations, _currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/ucsbdates/all"]
+    // );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'ID',
+            accessor: 'id',
+        },
+        {
+            Header: 'Requester Email',
+            accessor: 'requesterEmail',
+        },
+        {
+            Header: 'Professor Email',
+            accessor: 'professorEmail',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Date Requested',
+            accessor: 'dateRequested',
+        },
+        {
+            Header: 'Date Needed',
+            accessor: 'dateNeeded',
+        },
+        {
+            Header: 'Done?',
+            accessor: 'done',
+            accessor: (row, _rowIndex) => String(row.done)
+        }
+    ];
+
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+    //     ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
+    // ];
+
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = columns;
+
+    return <OurTable
+        data={recommendations}
+        columns={columnsToDisplay}
+        testid={"RecommendationsTable"}
+    />;
+};
