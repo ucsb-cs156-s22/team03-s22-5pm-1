12621adb52ee48b453b8145cd4917f1ae579975b
rishabhpoikayil <rishabh.poikayil@gmail.com>
2022-05-17 06:55:00 -0700

    rp - added placeholder for recommendations index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import DiningCommonsIndexPage from "main/pages/DiningCommons/DiningCommonsIndexP
 
 import OrganizationsIndexPage from "main/pages/Organizations/OrganizationsIndexPage";
 
+import RecommendationsIndexPage from "main/pages/Recommendations/RecommendationsIndexPage";
+
 import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
 import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
@@ -60,6 +62,13 @@ function App() {
           hasRole(currentUser, "ROLE_USER") && (
             <>
               <Route exact path="/organizations/list" element={<OrganizationsIndexPage />} />
+            </>
+          )
+        }
+        {
+          hasRole(currentUser, "ROLE_USER") && (
+            <>
+              <Route exact path="/recommendations/list" element={<RecommendationsIndexPage />} />
             </>
           )
         }
