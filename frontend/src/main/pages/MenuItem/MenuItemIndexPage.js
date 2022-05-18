73b0d44d90f7fa6eaf02f15fc784d28353eba480
Nga Ngo <nga@ucsb.edu>
2022-05-17 20:47:22 -0700

    nn - added placeholder page for menu item index

---
+++ frontend/src/main/pages/MenuItem/MenuItemIndexPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MenuItemIndexPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Menu Item</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
